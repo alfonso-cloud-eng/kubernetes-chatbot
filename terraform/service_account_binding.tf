3b3ad9fa8fe11a63925c061534b7d51a8bbe5a29
Alfonso Monserrat <alfonso.cloud.eng@gmail.com>
2025-03-13 10:27:47 +0100

    m

--- terraform/service_account_binding.tf
+++ terraform/service_account_binding.tf
@@ -1,7 +1,7 @@
 data "google_compute_default_service_account" "default" {}
 
 resource "google_service_account_iam_binding" "bind_deployer" {
-  service_account_id = data.google_compute_default_service_account.default.email
+  service_account_id = "projects/${var.gcp_project}/serviceAccounts/${data.google_compute_default_service_account.default.email}"
   role               = "roles/iam.serviceAccountUser"
 
   members = [
