d863b9df6475fb66653b0d64a1e90a15a5f9ad97
Alfonso Monserrat <alfonso.cloud.eng@gmail.com>
2025-03-13 08:51:18 +0100

    m

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,26 +7,10 @@ resource "google_container_cluster" "chatbot_cluster" {
   name     = var.cluster_name
   location = var.gcp_region
 
-  # Enable Autopilot mode
-  autopilot {
-    enabled = true
-  }
-
-    # For Autopilot clusters, you should remove the default node pool.
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  depends_on = [
-    google_project_service.enable_container_api
-  ]
-
   network    = "default"
   subnetwork = "default"
 
-  # Disable legacy ABAC
-  enable_legacy_abac = false
-
-  # Optional: set master authorized networks if desired
-  # master_authorized_networks_config { }
+  # Enabling Autopilot for this cluster
+  enable_autopilot = true
 }
 
