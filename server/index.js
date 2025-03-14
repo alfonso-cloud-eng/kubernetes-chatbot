fbb1bfd4707d2fd32a1907f546263ffcae61abf1
Alfonso Monserrat <alfonso.cloud.eng@gmail.com>
2025-03-14 11:10:08 +0100

    m

--- server/index.js
+++ server/index.js
@@ -48,6 +48,6 @@ app.post('/api/chat', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is listening on port ${PORT}`);
 });
