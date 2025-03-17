e2799f8a292631443652f01b365857bd17d3c75e
Alfonso Monserrat <alfonso.cloud.eng@gmail.com>
2025-03-17 10:58:56 +0100

    m

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,3 @@
-resource "google_project_service" "enable_container_api" {
-  project = var.gcp_project
-  service = "container.googleapis.com"
-}
-
-resource "google_project_service" "enable_cloudresourcemanager_api" {
-  project = var.gcp_project
-  service = "cloudresourcemanager.googleapis.com"
-}
-
-resource "google_project_service" "enable_compute_api" {
-  project = var.gcp_project
-  service = "compute.googleapis.com"
-}
-
 resource "google_container_cluster" "chatbot_cluster" {
   name     = var.cluster_name
   location = var.gcp_region
